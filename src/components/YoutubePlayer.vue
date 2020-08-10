43027d54a9974ac724cad181696c475160328f75
Joe Scot Marshall <joe.marshall@multiplytechnology.com>
2020-08-10 01:24:58 -0400

    added componenet

---
+++ src/components/YoutubePlayer.vue
@@ -0,0 +1,21 @@
+<template>
+  <youtube
+    style=""
+    :video-id="ytId(video.url)"
+    :player-vars="ytVars(video.url, 1)"
+    :mute="index !== currentIndex"
+    :player-width="$refs.home ? $refs.home.clientWidth : 350"
+    :player-height="$refs.home ? $refs.home.clientWidth * 0.5625 : 350 * 0.5625"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    video: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
